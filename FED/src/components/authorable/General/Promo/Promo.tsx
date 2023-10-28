6877ab26300823290548874db1ee1615dcdaa980
Tej Mehta <tmehta@horizontal.com>
2023-10-28 19:52:19 +0530

    ContentBlock code commented

--- FED/src/components/authorable/General/Promo/Promo.tsx
+++ FED/src/components/authorable/General/Promo/Promo.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import NextImage from 'next/image';
 import { Grid } from '@/components/helpers/Grid/Grid';
-const Promo = () => {
+
+const Promo = ({ fields }: any) => {
+  console.log(fields);
   return (
     <>
       <Grid variant="lg">
         <div className="inset-y-0 col-span-6 h-full">
           <div className="rounded-xl relative h-[240px]">
-            <NextImage src="https://picsum.photos/200/300" alt="Image" fill />
+            <NextImage
+              src="https://fastly.picsum.photos/id/496/200/300.jpg?hmac=demLRv0UMwDhQHH6AEmbkJqlYuX27lnRH5N9FYcHBgw"
+              alt="Image"
+              fill
+            />
           </div>
         </div>
         <div className="col-span-6 justify-between p-4 leading-normal">
-          <p className="pb-6 font-normal text-xl text-gray-900">
-            An advanced online playground for Tailwind CSS, including support for things likeAn
-            advanced online playground for Tailwind CSS, including support for things likeAn
-            advanced online playground for Tailwind CSS, including support for things likeAn
-          </p>
+          <p className="pb-6 font-normal text-xl text-gray-900">paragraph</p>
           <div>
-            <a href="https://tailwindcss.com/docs" className="text-sky-500 hover:text-sky-600">
-              Read the docs &rarr;
+            <a href="www.google.com" className="text-sky-500 hover:text-sky-600">
+              Read More
             </a>
           </div>
         </div>
