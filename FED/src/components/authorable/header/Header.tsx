ba715af3c444df64ac620dd978f789c1b2dbbb1c
HZI\draval <draval@horizontal.com>
2023-10-30 20:02:41 +0530

    Added news details

--- FED/src/components/authorable/header/Header.tsx
+++ FED/src/components/authorable/header/Header.tsx
@@ -20,38 +20,36 @@ const Header = ({ fields }: HeaderProps): JSX.Element => {
     <div className="bg-gray-150 ">
       <Grid variant="lg">
         <div className="logo md:col-span-4 relative h-[50]">
-          <Link  href={fields?.logoLink.value} passHref>
+          <Link href={fields?.logoLink?.value} passHref>
             <NextImage className="h-8 w-14" editable field={fields?.logo}></NextImage>
           </Link>
         </div>
-		<div className='md:col-span-8 ml-auto'>
-        <div className="block md:hidden h-5 w-5">
-          <button className="hamburger-icon text-black text-4xl" onClick={handleMenuClick}>
-            {isMenuOpen ? <RxCross2 /> : <GiHamburgerMenu />}
-          </button>
-        </div>
+        <div className="md:col-span-8 ml-auto">
+          <div className="block md:hidden h-5 w-5">
+            <button className="hamburger-icon text-black text-4xl" onClick={handleMenuClick}>
+              {isMenuOpen ? <RxCross2 /> : <GiHamburgerMenu />}
+            </button>
+          </div>
 
-        <div className={`${isMenuOpen ? 'block' : 'hidden'} md:block`}>
-          <ul className="flex flex-col text-center md:flex-row md:space-x-4 ml-auto">
-            {fields?.navLinks.map((navLink, index) => (
-              <li
-                key={index}
-                className="py-2 border-t-white border-t-2 border-opacity-30 md:border-none">
-					<Link key={index} href={navLink.url}  className="hover:no-underline text-black mr-4 font-sans">
-					{navLink.fields.NavigationTitle.value}
-					</Link>                
-              </li>
-            ))}
-          </ul>
+          <div className={`${isMenuOpen ? 'block' : 'hidden'} md:block`}>
+            <ul className="flex flex-col text-center md:flex-row md:space-x-4 ml-auto">
+              {fields?.navLinks.map((navLink, index) => (
+                <li
+                  key={index}
+                  className="py-2 border-t-white border-t-2 border-opacity-30 md:border-none"
+                >
+                  <Link
+                    key={index}
+                    href={navLink?.url}
+                    className="hover:no-underline text-black mr-4 font-sans"
+                  >
+                    {navLink?.fields?.NavigationTitle?.value}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-		</div>
-        {/* <nav className="col-span-8 ml-auto ">
-          {fields?.navLinks.map((navLink, index) => (
-            <a className="mr-4 font-sans" key={index} href={navLink.url}>
-              {navLink.fields.NavigationTitle.value}
-            </a>
-          ))}
-        </nav> */}
       </Grid>
     </div>
   );
